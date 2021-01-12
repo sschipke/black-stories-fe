228e86f090664866638be0bd70ef429e13374138
Scott <swschipke@gmail.com>
2021-01-11 18:26:36 -0700

    Remove unused link

--- src/components/MoviePage/MoviePage.js
+++ src/components/MoviePage/MoviePage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { openVideoPlayer, setCurrentMovie } from '../../actions';
