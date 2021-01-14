37d569230a220cc53ad323271cb46cfe66425bd9
Scott Schipke <swschipke@gmail.com>
2021-01-14 13:37:10 -0700

    Remove unused close icon

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { openVideoPlayer, closeVideoPlayer } from '../../actions';
-import closeIcon from '../../assets/icons/video_player_close.svg';
 import './VideoPlayer.scss';
 
 export const VideoPlayer = ({currentMovie, isOpen, closeVideoPlayer}) => {
