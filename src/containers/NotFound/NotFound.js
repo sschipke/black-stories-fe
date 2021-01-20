fea0582146117be6dd0a097e792f203894f41bef
Scott <swschipke@gmail.com>
2021-01-20 13:14:59 -0700

    Allow routing to specific movie based on URL

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -3,30 +3,25 @@ import { withRouter, Link, Redirect } from 'react-router-dom'
 import genreMap from '../../data/genreMap'
 import './NotFound.scss';
 
-const NotFound = ({match, history, location}) => {
-  console.log({match}, {history}, {location})
+const NotFound = ({ location }) => {
   const {pathname} = location
-  console.log(pathname)
   if(pathname.includes("/genre/")) {
     const params = pathname.split('/')
-    console.log(params)
     const genreId = params[2]
     const genreName = genreMap[genreId];
     if(!genreName) {
       return <Redirect to="/genres" />
     }
     console.log({genreId}, {genreName})
-    return <Redirect to={`./${genreId}-${genreName.toLowerCase().replace(' ', '_')}`} />
+    return <Redirect to={`/genre/${genreId}-${genreName.toLowerCase().replace(' ', '_')}`} />
   }
   if(pathname.includes("/movie/")) {
     const params = pathname.split('/')
-    console.log(params)
     const movieId = params[2]
-    console.log({movieId})
     if(!movieId) {
       return <div> Woops! Looks like you are lost, click below to return to the available genres: <Link to="/">Genres</Link></div>
     }
-    return <Redirect to={`./${movieId}-`} />
+    return <Redirect to={`/movie/${movieId}-`} />
   }
   return <div> Woops! Looks like you are lost, click below to return to the available genres: <Link to="/">Genres</Link></div>
 }
