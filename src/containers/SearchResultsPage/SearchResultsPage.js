7f8b910c9a200b96f02ed2c9f46e17d5697dfbd6
Scott <swschipke@gmail.com>
2021-03-15 12:00:12 -0600

    Add no search results elements

--- src/containers/SearchResultsPage/SearchResultsPage.js
+++ src/containers/SearchResultsPage/SearchResultsPage.js
@@ -6,11 +6,17 @@ import { setBackgroundClass, toggleSearchBar } from '../../actions';
 import MovieCard from '../../components/MovieCard/MovieCard';
 import './SearchResultsPage.scss';
 
-export const SearchResultsPage = ({ previouslyWatched, watchList, setBackgroundClass, searchText, toggleSearchBar}) => {
+export const SearchResultsPage = ({ previouslyWatched, watchList, setBackgroundClass, searchText}) => {
 
   useEffect(() => {
     setBackgroundClass("search-results-page");
   });
+
+  const noResults = <div className="no-results-div">
+    <h1>NO SEARCH RESULTS </h1>
+    <h1> SORRY, BABY</h1>
+    <img className="no-results-gif" src="https://4.bp.blogspot.com/-aRPiVblOJS8/WZ3fd7vph8I/AAAAAAAAocM/E_PRaQ-VuFYSksTXeZB0vUP8V6v9YW82gCLcBGAs/s1600/The%2BWiz.gif" alt="woman lounging on the couch in luxurious green clothing" style={{"font-size": "1.5rem;"}}/>
+  </div>
 
 const allMovies = [...previouslyWatched, ...watchList]
 
@@ -19,7 +25,7 @@ let searchResults = allMovies.filter(movie => movie.title.toLowerCase().includes
 
   switch (true) {
     case (searchResultsLength === 0):
-      searchResults = <p className="no-results">Sorry, couldn't find anything :sad-face:</p>
+      searchResults = noResults
       break;
     case searchResultsLength === 1:
       const singleMovie = searchResults[0];
