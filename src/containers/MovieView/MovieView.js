c0ba26461fa5c3acf82080b9eaf875245baced3f
Scott <swschipke@gmail.com>
2021-10-26 13:38:02 -0600

    Allow loading of individual movies

--- src/containers/MovieView/MovieView.js
+++ src/containers/MovieView/MovieView.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect }from 'react';
 import { Redirect } from 'react-router-dom'
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { setBackgroundClass} from '../../actions';
 import MoviePage from '../../components/MoviePage/MoviePage';
+import loadingGif from '../../assets/gifs/loading_fist.gif';
 import './MovieView.scss';
 
-const MovieView = ({movieId, unseenMovies, previousMovies}) => {
+const MovieView = ({movieId, unseenMovies, previousMovies, areMoviesLoaded, setBackgroundClass}) => {
   let currentMovie = unseenMovies.find(movie => movie.id === Number(movieId))
   if(!currentMovie) {
     currentMovie = previousMovies.find(movie => movie.id === Number(movieId))
   };
+
+  useEffect(() => {
+    setBackgroundClass("movie-view");
+  }, [setBackgroundClass])
+
+  if(!areMoviesLoaded && !currentMovie) {
+    return ( <div className="modal-background">
+      <div className="modal-content modal-loading-div">
+        Loading...
+        <img className="modal-loading-gif" src={loadingGif} alt="loading gif" />
+      </div>
+    </div>
+    )
+  }
 
   return (
     <div className="movie-container">
@@ -20,7 +37,11 @@ const MovieView = ({movieId, unseenMovies, previousMovies}) => {
 
 export const mapStateToProps = state => ({
   unseenMovies: state.data.watchList,
-  previousMovies: state.data.previouslySeen
+  previousMovies: state.data.previouslySeen,
+  areMoviesLoaded: state.data.areMoviesLoaded
 })
 
-export default connect(mapStateToProps)(MovieView);
+export const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ setBackgroundClass }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieView);
